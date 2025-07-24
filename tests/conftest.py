7462f70125305a428e26131fbd81ff3a2fbedfa4
Adam Kropp <30504149+akropp@users.noreply.github.com>
2025-07-24 14:21:07 -0400

    fix: pin pytest-asyncio to 0.21.0 and disable auto-enable custom integrations fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -23,10 +23,10 @@ pytest_plugins = "pytest_homeassistant_custom_component"
 
 # This fixture enables loading custom integrations in all tests.
 # Remove to enable selective use of this fixture
-@pytest.fixture(autouse=True)
-def auto_enable_custom_integrations(enable_custom_integrations):
-    """Enable custom integrations."""
-    yield
+# @pytest.fixture(autouse=True)
+# async def auto_enable_custom_integrations(enable_custom_integrations):
+#     """Enable custom integrations."""
+#     return
 
 
 # This fixture is used to prevent HomeAssistant from attempting to create and dismiss persistent
